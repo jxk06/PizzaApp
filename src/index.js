ae4b8d9ed6706186a67ffce5c0e37a16f5fdd455
Koh Jing Xi <24011503@myrp.edu.sg>
2025-11-04 09:12:57 +0800

    Lesson 5

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 function App() {
     return (
-        <div>
+        <div className="container">
             <Header />
+            <Menu />
+            <Footer />
+        </div>
+    )
+}
+
+function Menu() {
+    return (
+        <div className="menu">
+            <h2>Our Menu</h2>
             <Pizza />
             <Pizza2 />
         </div>
@@ -13,16 +24,39 @@ function App() {
 
 function Header() {
     return (
-        <h1>Jing Xi's Pizza Co.</h1>
+        <h1 style={{color: "orange", fontSize: "48px", textTransform: "uppercase"}}>Jing Xi's Pizza Co.</h1>
 
     )
 }
+
+function Footer() {
+    const currentHour = new Date().getHours();
+    const isOpen = currentHour >= 10 && currentHour <= 22;
+    return (
+        <footer className="footer">
+            {isOpen ? "We're currently open" : "Sorry we're closed"}
+        </footer>
+    );
+}
+
+//function Footer() {
+    //const currentHour = new Date().getHours();
+   // return (
+        //<footer className="footer">
+            //{currentHour >= 10 && currentHour < 22 ? "We're currently open" : "Sorry we're closed"}
+        //</footer>
+    //);
+//}
+
+
+
+
 
 function Pizza() {
     return (
         <div>
             <img src="/pizzas/focaccia.jpg" alt="focaccia" />
-             <h2> Focaccia</h2>
+             <h3> Focaccia</h3>
             <div>
                 <p>Olive oil</p>
                 <p>5</p>
@@ -39,7 +73,7 @@ function Pizza2() {
     return (
         <div>
             <img src="/pizzas/funghi.jpg" alt="funghi" />
-            <h2> Funghi Pizza</h2>
+            <h3> Funghi Pizza</h3>
             <div>
                 <p>Tomato, Onion, Cheese</p>
                 <p>10</p>
